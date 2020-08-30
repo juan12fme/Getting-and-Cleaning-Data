fe4acfd2dc55752b14e18e3b2821ec49a7a0cd2c
juan12fme <muchomedina.-23@hotmail.com>
2020-08-30 16:54:51 -0500

    a second tidy data set with average for each activity and subject

--- CodeBook.md.R
+++ CodeBook.md.R
@@ -32,3 +32,9 @@ names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
 names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+
+#a second tidy data set with average for each activity and subject
+FinalData <- TidyData %>%
+        group_by(subject, activity) %>%
+        summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name=FALSE)
