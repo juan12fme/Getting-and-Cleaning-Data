3fd916f92d2e74fc41b621cadc20316ed77904df
juan12fme <muchomedina.-23@hotmail.com>
2020-08-30 16:18:34 -0500

    merging train and test

--- CodeBook.md.R
+++ CodeBook.md.R
@@ -6,3 +6,14 @@ activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FAL
 subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
 activityTrain <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
 featuresTrain <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+
+#read test data
+subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+activityTest <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
+featuresTest <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+
+#merging train and test 
+subject<-rbind(subjectTrain, subjectTest)
+activity<- rbin(activityTrain, activityTest)
+features<- rbind(featuresTrain, featuresTest)
+colnames(features)<- t(featureNames[2])
